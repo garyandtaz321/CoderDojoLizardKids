1337d2738a200614e84146c0c4e9403f738362b3
kkuhn <kkuhn0000@hotmail.com>
2016-02-06 09:23:56 -0800

    PO-TA-TO

--- src/js/states/game.js
+++ src/js/states/game.js
@@ -5,11 +5,7 @@ var Game = function () {
 }
 
 var sprite;
-var Ene1;
-var Ene2;
-var Ene3;
-var Ene4;
-var Ene5;
+var Enemies;
 
 var group;
 var cursors;
@@ -21,7 +17,16 @@ module.exports = Game;
 Game.prototype = {
 
     create: function () {
+        Enemies = this.add.group();
 
+
+
+            Enemies.create( 100 , 436, 'ZAMBIE');
+        Enemies.create( 200 , 128, 'ZAMBIE');
+        Enemies.create( 300 , 345, 'ZAMBIE');
+        Enemies.create( 400 , 543, 'ZAMBIE');
+        Enemies.create( 500 , 245, 'ZAMBIE');
+        Enemies.create( 600 , 143, 'ZAMBIE');
 
 
 
@@ -29,55 +34,26 @@ Game.prototype = {
         sprite.enableBody = true;
         this.physics.startSystem(Phaser.Physics.ARCADE);
         sprite = this.add.sprite(567, 400, 'Player');
-        Ene1 = this.add.sprite(100, 156, 'ZAMBIE');
-        Ene2 = this.add.sprite(200, 235, 'ZAMBIE');
-        Ene3 = this.add.sprite(300, 345, 'ZAMBIE');
-        Ene4 = this.add.sprite(600, 523, 'ZAMBIE');
-        Ene5 = this.add.sprite(750, 456, 'ZAMBIE');
+
         this.physics.enable(sprite , Phaser.Physics.ARCADE);
-        this.physics.enable(Ene1 , Phaser.Physics.ARCADE);
-        this.physics.enable(Ene2 , Phaser.Physics.ARCADE);
-        this.physics.enable(Ene3 , Phaser.Physics.ARCADE);
-        this.physics.enable(Ene4 , Phaser.Physics.ARCADE);
-        this.physics.enable(Ene5 , Phaser.Physics.ARCADE);
+        this.physics.enable(Enemies , Phaser.Physics.ARCADE);
+
 
         sprite.body.collideWorldBounds=true;
 
         this.physics.arcade.enable(sprite);
-        this.physics.arcade.enable(Ene1);
-        this.physics.arcade.enable(Ene2);
-        this.physics.arcade.enable(Ene3);
-        this.physics.arcade.enable(Ene4);
-        this.physics.arcade.enable(Ene5);
+        this.physics.arcade.enable(Enemies);
+
         sprite.body.setSize(220, 10, 0, 0);
-        Ene1.body.setSize(220, 10, 0, 0);
-        Ene2.body.setSize(220, 10, 0, 0);
-        Ene3.body.setSize(220, 10, 0, 0);
-        Ene4.body.setSize(220, 10, 0, 0);
-        Ene5.body.setSize(220, 10, 0, 0);
         cursors = this.input.keyboard.createCursorKeys();
-        right = this.input.keyboard.addKey(Phaser.Keyboard.A);
-        left = this.input.keyboard.addKey(Phaser.Keyboard.D);
+
 
     },
 
     update: function () {
         sprite.body.velocity.x = 0;
         sprite.body.velocity.y = 0;
-        this.physics.arcade.collide(Player, Ene1, this.collisionHandler, null, this);
-        this.physics.arcade.collide(Player, Ene2, this.collisionHandler, null, this);
-        this.physics.arcade.collide(Player, Ene3, this.collisionHandler, null, this);
-        this.physics.arcade.collide(Player, Ene4, this.collisionHandler, null, this);
-        this.physics.arcade.collide(Player, Ene5, this.collisionHandler, null, this);
 
-        Ene1.angle += 1;
-
-        Ene2.angle += 1;
-
-        Ene3.angle += 1;
-
-        Ene4.angle += 1;
-        Ene5.angle += 1;
 
 
 
